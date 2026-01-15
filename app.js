57f523f2d8727fbeec152d318790beeb5ac19333
Vinay <vnhrmth@gmail.com>
2026-01-15 11:03:27 +0530

    added controller, data and models

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path: null});
-});
+app.use(errorController.get404Error);
 
 app.listen(3000);
